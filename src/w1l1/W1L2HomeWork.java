a72b6e3a8a2aa19a0c3fb64be6a129703570783c
Kibrom1 <kibmit@gmail.com>
2015-11-23 18:11:18 -0600

    Commited again

--- src/w1l1/W1L2HomeWork.java
+++ src/w1l1/W1L2HomeWork.java
@@ -5,6 +5,7 @@ public class W1L2HomeWork {
 	public static void main(String[] args){
 		
 		System.out.println("This is the first trial");
+		System.out.println("It helps to remove the life!");
 	}
 
 }

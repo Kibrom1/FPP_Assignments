bbe3a4d2445908f1c88c9e9a7dd5365bb3d22ea3
unknown <984763@CS_LAB113-205.cs.mum.edu>
2015-11-24 14:12:03 -0600

    Modified lesson2

--- src/week1lesson2/Week1Lesson2.java
+++ src/week1lesson2/Week1Lesson2.java
@@ -2,8 +2,13 @@ package week1lesson2;
 
 public class Week1Lesson2 {
 
+	public static String[] RemoveDups(String[] arr){
+		
+		return arr;
+	}
 	public static void main(String[] args) {
-
+		String[] arr = {"horse", "dog", "cat", "horse","dog"};
+		System.out.println(RemoveDups(arr).toString());
 		System.out.println("In second lesson!");
 	}
 }
